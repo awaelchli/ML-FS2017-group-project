bdb37d16c99fe8a3020ba1fe95f465a62d4e204d
Phyranja <vita@students.unibe.ch>
2017-04-06 14:31:54 +0200

    The recurrent net seems to work now.

--- recurrent.lua
+++ recurrent.lua
@@ -1,18 +1,28 @@
-require 'nngraph'
+--require 'nngraph'
 require 'torch'
 require 'nn'
 require 'rnn'
 require 'dpnn'
 require 'optim'
+require 'load_images'
 
 -- define inputs
-local x1 = torch.rand(5,5,3)
+--local x1 = torch.rand(3,5,5)
+
+images = load_images.load('datasets/Set14/image_SRF_4/', 'png', false)
+inputChannels = 3
+
+-- Convert greyscale images
+for i = 1, #images do
+        images[i] = images[i]:expand(inputChannels, images[i]:size(2), images[i]:size(3))
+end
+x1 = images[1]
 
 
 -- model implementation, define an MLP
 
 
-local input = nn.Identity()():annotate{name = 'Input'}
+local input = nn.Identity()
 inputChannels = 3 -- RGB
 kernelSize = 3
 step = 1
@@ -22,11 +32,12 @@ inner = nn.Sequential()
 inner:add(nn.SpatialConvolution(inputChannels, inputChannels, kernelSize, kernelSize, step, step, 1, 1))
 inner:add(nn.ReLU())
 
-c = input:sharedClones()
+--print(input)
+--print(inner)
 
-recurrent = nn.Recurrent(inner, input, nn.Identity()())
+recurrent = nn.Recurrent(inner, input, nn.Identity(),nn.Identity(), 1)
 
-out = identity()()
+out = nn.Identity()
 recurrent:add(out)
 
 
@@ -34,12 +45,13 @@ recurrent:add(out)
 
 --local mlp = nn.Sequential()
 --mlp:add(h)
-mlp = nn.gModule({input},{out})
+
 
 
 
 
 -- print the size of your outputs
-outputs = mlp:forward({x1})
-graph.dot(mlp.fg, 'Graph','test_recurrent')
+outputs = recurrent:forward(x1)
+--graph.dot(mlp.fg, 'Graph','test_recurrent')
 print(outputs:size())
+image.save('rec_test.png', outputs)
