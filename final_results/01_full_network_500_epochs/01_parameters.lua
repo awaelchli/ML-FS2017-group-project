34d92c9f14320c3e348e7cf3926b2eb2f917303b
Adrian <adrian.waelchli@students.unibe.ch>
2017-05-13 02:34:53 -0700

    added results of first successful training

---
+++ final_results/01_full_network_500_epochs/01_parameters.lua
@@ -0,0 +1,19 @@
+require 'parameters.TEMPLATE'
+
+-----------------------------------
+-- Parameters to be tuned freely --
+-----------------------------------
+actionParam.upscaleFactor = 4
+actionParam.numRecursions = 5
+actionParam.numHiddenChannelsInRecursion = 6
+
+actionParam.epochs = 100
+actionParam.sgd_params = {
+   learningRate = 1e-1,
+   learningRateDecay = 1e-8,
+   weightDecay = 0,
+   momentum = 0--0.9
+}
+
+actionParam.mainData = {'datasets/BSD100_SR/image_SRF_4/','datasets/Set14/image_SRF_4/'}
+actionParam.testData = {'datasets/Set5/image_SRF_4/'}
