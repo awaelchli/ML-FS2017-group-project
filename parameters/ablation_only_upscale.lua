22cc0d7eb80dd15659565e07020d5632508de16c
awaelchli <adrian.waelchli@students.unibe.ch>
2017-05-15 14:07:57 +0200

    separate folder for output of different experiments

--- parameters/ablation_only_upscale.lua
+++ parameters/ablation_only_upscale.lua
@@ -7,16 +7,16 @@ actionParam.networkFile = 'build_network_ABL_upscale_only'
 -- Parameters to be tuned freely --
 -----------------------------------
 actionParam.upscaleFactor = 4
-actionParam.numRecursions = 5
-actionParam.numHiddenChannelsInRecursion = 6
 
-actionParam.epochs = 100
+actionParam.epochs = 500
 actionParam.sgd_params = {
    learningRate = 1e-1,
-   learningRateDecay = 1e-4,
+   learningRateDecay = 1e-8,
    weightDecay = 0,
    momentum = 0
 }
 
 actionParam.mainData = {'datasets/BSD100_SR/image_SRF_4/','datasets/Set14/image_SRF_4/'}
 actionParam.testData = {'datasets/Set5/image_SRF_4/'}
+
+actionParam.folders.testResults = actionParam.folders.output .. 'results/upscale_only/'
--- parameters/full_network.lua
+++ parameters/full_network.lua
@@ -7,7 +7,7 @@ actionParam.upscaleFactor = 4
 actionParam.numRecursions = 5
 actionParam.numHiddenChannelsInRecursion = 6
 
-actionParam.epochs = 100
+actionParam.epochs = 500
 actionParam.sgd_params = {
    learningRate = 1e-1,
    learningRateDecay = 1e-8,
@@ -17,3 +17,5 @@ actionParam.sgd_params = {
 
 actionParam.mainData = {'datasets/BSD100_SR/image_SRF_4/','datasets/Set14/image_SRF_4/'}
 actionParam.testData = {'datasets/Set5/image_SRF_4/'}
+
+actionParam.folders.testResults = actionParam.folders.output .. 'results/full_network/'
